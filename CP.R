4ed962f36de7420458830ba42da6f3a520622059
firdausai <firdaus.indradhirmaya@gmail.com>
2019-05-04 02:20:28 +0700

    Closest pair with devide and conquere method v2

--- CP.R
+++ CP.R
@@ -12,23 +12,26 @@ calculateDistance <- function(x1,x2,y1,y2) {
 bruteForce <- function(table) {
   i = 2
   n = i + 1
-  min = calculateDistance(table[1,1],table[2,1],table[1,2],table[2,2])
+  min = calculateDistance(table$X[[1]],table$X[[2]],table$Y[[1]],table$Y[[2]])
+  dataFrame = data_frame("Distance" = c(min,min), "X" = c(table$X[[1]],table$X[[2]]), "Y" = c(table$Y[[1]],table$Y[[2]]))
   while(i < nrow(table)) {
     n = i + 1
     while(n < nrow(table)) {
-      x1 <- table[i,1]
-      x2 <- table[n,1]
-      y1 <- table[i,2]
-      y2 <- table[n,2]
+      x1 <- table$X[[i]]
+      x2 <- table$X[[n]]
+      y1 <- table$Y[[i]]
+      y2 <- table$Y[[n]]
+      
       comparedValue <- calculateDistance(x1,x2,y1,y2)
       if(min > comparedValue) {
         min <- comparedValue
+        dataFrame = data_frame("Distance" = c(min,min), "X" = c(x1,x2), "Y" = c(y1,y2))
       }
       n = n + 1
     }
     i = i + 1
   }
-  return(min)
+  return(dataFrame)
 }
 
 compareMinValue <- function(val1,val2) {
@@ -62,12 +65,12 @@ RPoints <- filter(sortedX, X > middleLine)
 
 #Calculate shortest distance left & right side
 shortestLeft <- bruteForce(LPoints)
-shortestLeft <- shortestLeft$X[[1]]
+shortestLeftDistance <- shortestLeft$Distance[[1]]
 shortestRight <- bruteForce(RPoints)
-shortestRight <- shortestRight$X[[1]]
+shortestRightDistance <- shortestRight$Distance[[1]]
 
 #Determine which side has the shortest distance between points
-minDistance <- compareMinValue(shortestLeft,shortestRight)
+minDistance <- compareMinValue(shortestLeftDistance,shortestRightDistance)
 
 #Collect points within shortest distance range from the middle
   #Has to be able to compare like value1 < x < value2
@@ -76,17 +79,28 @@ MPoints <- filter(sortedX, X < (middleLine+minDistance))
 MPoints <- filter(MPoints, X > (middleLine-minDistance))
 
 #Calculate shortest distance from the middle
-if (nrow(MPoints) != 1) {
+if (nrow(MPoints) > 1) {
   shortestMiddle <- bruteForce(MPoints)
-  shortestMiddle <- shortestMiddle$X[[1]]
+  shortestMiddleDistance <- shortestMiddle$Distance[[1]]
+} else {
+  shortestMiddleDistance <- NULL
 }
 
-
 #Determine shortest distance (Compare shortest distance from the middle and left or right)
-if (nrow(MPoints) != 1) {
-  finalSmallestDistance <- compareMinValue(minDistance,shortestMiddle)
+if (nrow(MPoints) > 1) {
+  finalSmallestDistance <- compareMinValue(minDistance,shortestMiddleDistance)
   finalSmallestDistance
 } else {
   finalSmallestDistance <- minDistance
   minDistance
+}
+
+if(shortestLeftDistance < shortestRightDistance) {
+  graph <- graph + geom_point(aes(x = shortestLeft$X[[1]],y = shortestLeft$Y[[1]]),color = "red")
+  graph <- graph + geom_point(aes(x = shortestLeft$X[[2]],y = shortestLeft$Y[[2]]),color = "red")
+  graph
+} else {
+  graph <- graph + geom_point(aes(x = shortestRight$X[[1]],y = shortestRight$Y[[1]]),color = "red")
+  graph <- graph + geom_point(aes(x = shortestRight$X[[2]],y = shortestRight$Y[[2]]),color = "red")
+  graph
 }
